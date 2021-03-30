78bdef365896e210efb7e3631aae7ead7eea8bca
Gurvinder Singh <gurvinderscs@gmail.com>
2021-03-30 14:58:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/googlemap/models/Draggable.java
@@ -0,0 +1,48 @@
+package com.example.googlemap.models;
+
+import android.os.RemoteException;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.RuntimeRemoteException;
+
+public interface Draggable {
+
+    boolean onMarkerMoveStart(Marker marker);
+
+    boolean onMarkerMove(Marker marker);
+
+    void onEditStart();
+
+    void onEditEnd();
+
+    void onEditCancel();
+
+    void setZIndex(float index);
+
+    float getZIndex();
+
+    void setVisible(boolean visible);
+
+    boolean isVisible();
+
+    void setDraggable(boolean draggable);
+
+    boolean isDraggable();
+
+    void setCenter(LatLng position);
+
+    LatLng getCenter();
+
+    void setCenterAnchor(float x, float y);
+
+    void setTitle(String title);
+
+    String getTitle();
+
+    void setId(long id);
+
+    long getId();
+
+    void remove();
+}
